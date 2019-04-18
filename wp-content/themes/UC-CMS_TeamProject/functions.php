1c0992cd0a987da1cd80c6b9be2d038ef050a1e7
kimbah <kim8hughes@iinet.net.au>
2019-04-18 17:22:28 +0800

    added comments to post, used flexbox for gallery

--- wp-content/themes/UC-CMS_TeamProject/functions.php
+++ wp-content/themes/UC-CMS_TeamProject/functions.php
@@ -37,6 +37,8 @@ function custom_excerpt_length() {
 
 add_filter('excerpt_length', 'custom_excerpt_length');
 
+// Activate HTML5 features
+add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
 
 // Activating Widgets
 
